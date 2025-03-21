da629badaba60fd44cfe83b80875ef2f0c4f3cf2
rifkifi <rifkifi@gmail.com>
2025-03-21 13:19:54 +0100

    Update README.md

--- src/main.py
+++ src/main.py
@@ -17,8 +17,8 @@ def main():
     engine = Engine()
     
     player1 = Player(1, COLORS[0], True, AStarAI_h2(engine))
-    player2 = Player(2, COLORS[1], True, AStarAI_h2(engine))
-    max_matches = 2
+    player2 = Player(2, COLORS[1], True, LookAhead_h2(engine))
+    max_matches = 3
     
     result_path = "../result/{}_vs_{}.csv".format(player1.name, player2.name)
     
@@ -26,9 +26,7 @@ def main():
     running = True
     while running:
         # clock.tick(FPS)
-        # if not game.end:
-            # running = game.loop()
-            running = game.loop()
+        running = game.loop()
          
     pygame.quit()
     sys.exit()
