e49d8965de45c1ce8d721a63c227db276757f905
rifkifi <rifkifi@gmail.com>
2025-03-22 09:56:07 +0100

    Remove duplicate class

--- src/AI.py
+++ src/AI.py
@@ -178,234 +178,181 @@ class LookAhead_h2:
         best_move = possible_moves[np.random.choice(best_moves_index[0])] # Pick random move from best moves if it more than 1
         return self._engine.update_state(best_move)
     
-# Algorithm that looks ahead one more move for each possible move.
-class LookAhead_h2:
-    def  __init__(self, engine: Engine):
-        n, gs = 9, 4
-        self._engine: Engine = engine
-        self.heuristic = np.sum(np.mgrid[0:n, 0:n][:, ::-1, :],axis=0)
-        self.evaluation = 0
-        
-    def move(self):
-        if self._engine.game_state[2]:
-            heuristic = self.heuristic.T
-        else:
-            heuristic = self.heuristic
-        
-        possible_moves = self._engine.results(self._engine.actions())
-        possible_moves_heuristics = np.sum(heuristic * possible_moves, axis=(1, 2))
-
-        # save current engine state for resetting
-        cur_p1_mask = self._engine._p1_mask
-        cur_p2_mask = self._engine._p2_mask
-        cur_turn = self._engine.is_p2_turn
-        cur_turn_count = self._engine.turn_count
-
-        # iterate through possible moves
-        for i, move in enumerate(possible_moves):
-
-            # check if move achieves goal state
-            if self._engine.update_state(move):
-                # if it does, reset effects of update_state and return updated state
-                self._engine._p1_mask = cur_p1_mask
-                self._engine._p2_mask = cur_p2_mask
-                self._engine.is_p2_turn = cur_turn
-                self._engine.turn_count = cur_turn_count
-                return self._engine.update_state(move)
-            # if not, make the engine think it is our move again and get possible moves
-            else:
-                self._engine.is_p2_turn = cur_turn
-                self._engine.turn_count = cur_turn_count
-                possible_next_moves = self._engine.results(self._engine.actions())
-                next_move_heuristics = np.sum(heuristic * possible_next_moves, axis=(1, 2))
-                possible_moves_heuristics[i] += np.min(next_move_heuristics)
-
-            # restore state to our original turn
-            self._engine._p1_mask = cur_p1_mask
-            self._engine._p2_mask = cur_p2_mask
-            self._engine.is_p2_turn = cur_turn
-            self._engine.turn_count = cur_turn_count
-
-        self.evaluation = possible_moves_heuristics
-        best_moves_index = np.where(self.evaluation == np.min(self.evaluation))
-        best_move = possible_moves[np.random.choice(best_moves_index[0])] # Pick random move from best moves if it more than 1
-        return self._engine.update_state(best_move)
+# class  MCTSAI: # To Do
+#     def  __init__(self, engine, explorationWeight=0.5, iterations=500):
+#         n, gs = 9, 4
+#         self._engine = engine
+#         self.heuristic = np.sum(np.mgrid[0:n, 0:n][:, ::-1, :],axis=0)
+#         self.cost = 0
+#         self.evaluation = 0
+#         self.explorationWeight = explorationWeight
+#         self.iterations = iterations
     
-class  MCTSAI: # To Do
-    def  __init__(self, engine, explorationWeight=0.5, iterations=500):
-        n, gs = 9, 4
-        self._engine = engine
-        self.heuristic = np.sum(np.mgrid[0:n, 0:n][:, ::-1, :],axis=0)
-        self.cost = 0
-        self.evaluation = 0
-        self.explorationWeight = explorationWeight
-        self.iterations = iterations
-    
-    class Node:
-        def __init__(self, state, parent=None):
-            self.initial_state = state
-            self.state = state
-            self.parent = parent
-            self.player = self.state[2]
-            self.children = {}
-            self.visits = 0
-            self.reward = 0
-            self.e = Engine()
-            self.e.p1_mask = self.state[0]
-            self.e.p2_mask = self.state[1]
-            self.e.is_p2_turn = self.state[2]
-            n, gs = 9, 4
-            self.heuristic = np.sum(np.mgrid[0:n, 0:n][:, ::-1, :],axis=0)
+#     class Node:
+#         def __init__(self, state, parent=None):
+#             self.initial_state = state
+#             self.state = state
+#             self.parent = parent
+#             self.player = self.state[2]
+#             self.children = {}
+#             self.visits = 0
+#             self.reward = 0
+#             self.e = Engine()
+#             self.e.p1_mask = self.state[0]
+#             self.e.p2_mask = self.state[1]
+#             self.e.is_p2_turn = self.state[2]
+#             n, gs = 9, 4
+#             self.heuristic = np.sum(np.mgrid[0:n, 0:n][:, ::-1, :],axis=0)
 
-        def isFullyExpanded(self):
-            # return len(self.children) == len(self.e.results(self.e.actions()))
-            return len(self.children) == len(self.getBestLegalMoves())
+#         def isFullyExpanded(self):
+#             # return len(self.children) == len(self.e.results(self.e.actions()))
+#             return len(self.children) == len(self.getBestLegalMoves())
         
-        def isGoal(self):
-            return self.e.is_goal()
+#         def isGoal(self):
+#             return self.e.is_goal()
         
-        def getLegalMoves(self):
-            return self.e.results(self.e.actions())
+#         def getLegalMoves(self):
+#             return self.e.results(self.e.actions())
         
-        def getBestLegalMoves(self):
-            heuristic = np.sum(self.heuristic * self.getLegalMoves(), axis=(1,2))    
-            best_moves_index = np.where(heuristic == np.min(heuristic))[0]
-            best_moves = np.empty((0,) + self.getLegalMoves().shape[1:], dtype=self.getLegalMoves().dtype)
-            for x in best_moves_index:
-                best_moves = np.append(best_moves, [self.getLegalMoves()[x]], axis=0)
-            return best_moves
+#         def getBestLegalMoves(self):
+#             heuristic = np.sum(self.heuristic * self.getLegalMoves(), axis=(1,2))    
+#             best_moves_index = np.where(heuristic == np.min(heuristic))[0]
+#             best_moves = np.empty((0,) + self.getLegalMoves().shape[1:], dtype=self.getLegalMoves().dtype)
+#             for x in best_moves_index:
+#                 best_moves = np.append(best_moves, [self.getLegalMoves()[x]], axis=0)
+#             return best_moves
         
-        def updateState(self, move):
-            self.e.update_state(move)
-            self.state = self.e.game_state
-            self.player = self.state[2]
+#         def updateState(self, move):
+#             self.e.update_state(move)
+#             self.state = self.e.game_state
+#             self.player = self.state[2]
         
-        def setToInitialState(self):
-            self.e.p1_mask = self.initial_state[0]
-            self.e.p2_mask = self.initial_state[1]
-            self.e.is_p2_turn = self.initial_state[2]
-            self.state = self.e.game_state
+#         def setToInitialState(self):
+#             self.e.p1_mask = self.initial_state[0]
+#             self.e.p2_mask = self.initial_state[1]
+#             self.e.is_p2_turn = self.initial_state[2]
+#             self.state = self.e.game_state
     
-    def move(self):
-        e = Engine()
-        e.p1_mask = self._engine.p1_mask
-        e.p2_mask = self._engine.p2_mask
-        e.is_p2_turn = self._engine.is_p2_turn
-        root_node = self.Node(e.game_state)
-        self.mcts(root_node)
-        i = 0
-        # for child in root_node.children.values():
-        #     print(f"{i}: visits: {child.visits} reward: {child.reward}")
-        #     i += 1
-            # print(f"winrate: {child.reward / child.visits}")
-        # sys.exit()
-        best_move = max(root_node.children.items(), key=lambda item: item[1].reward)[0]
-        # print(len(root_node.children.items()))
-        # sys.exit()
-        # best_move = max(
-        #     root_node.children.items(),
-        #     key=lambda item: (item[1].reward / item[1].visits if item[1].visits != 0 else -float('inf'))
-        # )[0]
-        # print(f"best move: {best_move}")
-        return self._engine.update_state(np.array(best_move))
+#     def move(self):
+#         e = Engine()
+#         e.p1_mask = self._engine.p1_mask
+#         e.p2_mask = self._engine.p2_mask
+#         e.is_p2_turn = self._engine.is_p2_turn
+#         root_node = self.Node(e.game_state)
+#         self.mcts(root_node)
+#         i = 0
+#         # for child in root_node.children.values():
+#         #     print(f"{i}: visits: {child.visits} reward: {child.reward}")
+#         #     i += 1
+#             # print(f"winrate: {child.reward / child.visits}")
+#         # sys.exit()
+#         best_move = max(root_node.children.items(), key=lambda item: item[1].reward)[0]
+#         # print(len(root_node.children.items()))
+#         # sys.exit()
+#         # best_move = max(
+#         #     root_node.children.items(),
+#         #     key=lambda item: (item[1].reward / item[1].visits if item[1].visits != 0 else -float('inf'))
+#         # )[0]
+#         # print(f"best move: {best_move}")
+#         return self._engine.update_state(np.array(best_move))
     
-    def mcts(self, root_node):
-        for _ in range(self.iterations):
-            selected_node = self.select(root_node)
+#     def mcts(self, root_node):
+#         for _ in range(self.iterations):
+#             selected_node = self.select(root_node)
             
-            if selected_node.isGoal():
-                if root_node.player != selected_node.player:
-                    reward = 1
-                else:
-                    reward = 0
-            else:
-                reward = self.simulate(root_node, selected_node)
-                # print(f"reward: {reward}")
-            self.backpropagate(root_node,selected_node, reward)
-            # print(f"iteration {_}, reward: {root_node.reward}")
-        # sys.exit()
+#             if selected_node.isGoal():
+#                 if root_node.player != selected_node.player:
+#                     reward = 1
+#                 else:
+#                     reward = 0
+#             else:
+#                 reward = self.simulate(root_node, selected_node)
+#                 # print(f"reward: {reward}")
+#             self.backpropagate(root_node,selected_node, reward)
+#             # print(f"iteration {_}, reward: {root_node.reward}")
+#         # sys.exit()
         
-    def select(self, node):
-        while not node.isGoal():
-            if not node.isFullyExpanded():
-                return self.expand(node)
-            else:
-                node = self.bestChild(node)
-        return node
+#     def select(self, node):
+#         while not node.isGoal():
+#             if not node.isFullyExpanded():
+#                 return self.expand(node)
+#             else:
+#                 node = self.bestChild(node)
+#         return node
 
-    def expand(self, node):
-        # legal_moves = node.getLegalMoves()
-        legal_moves = node.getBestLegalMoves()
-        for move in legal_moves:
-            move_tuple = tuple(map(tuple, move))  # Convert numpy.ndarray to tuple
-            if move_tuple not in node.children:
-                node.updateState(move)
-                child_node = self.Node(node.state, parent=node)
-                node.children[move_tuple] = child_node
-                node.setToInitialState()
-                # sys.exit()
-                return child_node
-        return None
+#     def expand(self, node):
+#         # legal_moves = node.getLegalMoves()
+#         legal_moves = node.getBestLegalMoves()
+#         for move in legal_moves:
+#             move_tuple = tuple(map(tuple, move))  # Convert numpy.ndarray to tuple
+#             if move_tuple not in node.children:
+#                 node.updateState(move)
+#                 child_node = self.Node(node.state, parent=node)
+#                 node.children[move_tuple] = child_node
+#                 node.setToInitialState()
+#                 # sys.exit()
+#                 return child_node
+#         return None
 
-    def bestChild(self, node):
-        max_uct = -float('inf')
-        best = None
-        for child in node.children.values():
-            if child.visits == 0:
-                uct = float('inf')
-            else:
-                exploitation = child.reward / child.visits
-                exploration = self.explorationWeight * math.sqrt(math.log(node.visits) / child.visits)
-                uct = exploitation + exploration
-            if uct > max_uct:
-                max_uct = uct
-                best = child
-        return best
+#     def bestChild(self, node):
+#         max_uct = -float('inf')
+#         best = None
+#         for child in node.children.values():
+#             if child.visits == 0:
+#                 uct = float('inf')
+#             else:
+#                 exploitation = child.reward / child.visits
+#                 exploration = self.explorationWeight * math.sqrt(math.log(node.visits) / child.visits)
+#                 uct = exploitation + exploration
+#             if uct > max_uct:
+#                 max_uct = uct
+#                 best = child
+#         return best
     
-    def simulate(self, root_node, selected_node, max_depth=100):
-        depth = 0
-        while not selected_node.isGoal():
-            if selected_node.player:
-                heuristic = self.heuristic.T
-            else:
-                heuristic = self.heuristic
+#     def simulate(self, root_node, selected_node, max_depth=100):
+#         depth = 0
+#         while not selected_node.isGoal():
+#             if selected_node.player:
+#                 heuristic = self.heuristic.T
+#             else:
+#                 heuristic = self.heuristic
                 
-            if depth >= max_depth:
-                # print("Max Depth Reached")
-                if root_node.player:
-                    player_dist = np.sum(heuristic[None] * selected_node.state[1])
-                    opponent_dist = np.sum(heuristic[None]  * selected_node.state[0])
-                    return 1 if player_dist < opponent_dist else 0
-                else:
-                    player_dist = np.sum(heuristic[None] * selected_node.state[0])
-                    opponent_dist = np.sum(heuristic[None]  * selected_node.state[1])
-                    return 1 if player_dist < opponent_dist else 0
+#             if depth >= max_depth:
+#                 # print("Max Depth Reached")
+#                 if root_node.player:
+#                     player_dist = np.sum(heuristic[None] * selected_node.state[1])
+#                     opponent_dist = np.sum(heuristic[None]  * selected_node.state[0])
+#                     return 1 if player_dist < opponent_dist else 0
+#                 else:
+#                     player_dist = np.sum(heuristic[None] * selected_node.state[0])
+#                     opponent_dist = np.sum(heuristic[None]  * selected_node.state[1])
+#                     return 1 if player_dist < opponent_dist else 0
             
-            legal_moves = selected_node.getLegalMoves()
+#             legal_moves = selected_node.getLegalMoves()
       
-            heuristic_all = np.sum(heuristic * legal_moves, axis=(1,2))
-            best_moves_index = np.where(heuristic_all == np.min(heuristic_all))
-            move = legal_moves[np.random.choice(best_moves_index[0])] 
-            selected_node.updateState(move)
-            depth = depth + 1
-            # print()
-            # print(f"depth {depth} of selected_node")
-            # print(selected_node.state[0])
-            # print(selected_node.state[1])
-            # print(selected_node.state[2])
+#             heuristic_all = np.sum(heuristic * legal_moves, axis=(1,2))
+#             best_moves_index = np.where(heuristic_all == np.min(heuristic_all))
+#             move = legal_moves[np.random.choice(best_moves_index[0])] 
+#             selected_node.updateState(move)
+#             depth = depth + 1
+#             # print()
+#             # print(f"depth {depth} of selected_node")
+#             # print(selected_node.state[0])
+#             # print(selected_node.state[1])
+#             # print(selected_node.state[2])
             
-        if root_node.player != selected_node.player:
-            # print("P2 Wins")
-            return 1
-        else:
-            # print("P1 Wins")
-            return 0
+#         if root_node.player != selected_node.player:
+#             # print("P2 Wins")
+#             return 1
+#         else:
+#             # print("P1 Wins")
+#             return 0
 
-    def backpropagate(self, root_node, selected_node, reward):
-        while selected_node is not None:
-            selected_node.visits += 1
-            selected_node.reward += reward
-            selected_node = selected_node.parent
+#     def backpropagate(self, root_node, selected_node, reward):
+#         while selected_node is not None:
+#             selected_node.visits += 1
+#             selected_node.reward += reward
+#             selected_node = selected_node.parent
     
 
 # class SortaGreedyTreeSearchAI: # WIP
