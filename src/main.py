cf904394d5808f84a69a0a9c65bad0aed55a30a7
Josh LeBlanc <joshcapitals07@gmail.com>
2025-03-23 13:17:39 +0100

    added minimax with h1

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ def main():
     clock = pygame.time.Clock()
     engine = Engine()
     
-    player1 = Player(1, COLORS[0], True, LookAhead_h2(engine))
+    player1 = Player(1, COLORS[0], True, MiniMax_h1(engine))
     player2 = Player(2, COLORS[1], True, MiniMax_h2(engine))
     max_matches = 10
     
